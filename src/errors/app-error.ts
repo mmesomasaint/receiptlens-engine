d37f19fc87a74b8c69d219271234d57d3eac7c14
Mmesoma Saint Echezona <104903171+mmesomasaint@users.noreply.github.com>
2026-08-19 21:26:24 +0100

    Add AppError class for error handling

--- src/errors/app-error.ts
+++ src/errors/app-error.ts
@@ -1,1 +1,12 @@
+// src/errors/app-error.ts
+export class AppError extends Error {
+  public statusCode: number;
+  public isOperational: boolean;
 
+  constructor(message: string, statusCode = 500) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
