b2fb5fbee9a365084f3ff6f5780253ba0fbb671e
Mmesoma Saint Echezona <104903171+mmesomasaint@users.noreply.github.com>
2026-08-19 21:05:12 +0100

    Configure environment for test setup
    Set environment variables for testing.

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,5 @@
-
+// tests/setup.ts
+process.env.NODE_ENV = 'test';
+process.env.PORT = '8001';
+process.env.JWT_SECRET = 'test_jwt_secret_key_receiptlens_998822';
+process.env.ENCRYPTION_KEY = '0123456789abcdef0123456789abcdef';
